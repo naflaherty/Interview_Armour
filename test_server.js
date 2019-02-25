7876a48ab836a208cb9d79caf3525e32ecbb1ac3
naflaherty <47085569+naflaherty@users.noreply.github.com>
2019-02-25 11:11:11 +0000

    Add files via upload

---
+++ test_server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }  
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080); 
+
+
+//var http = require('http').createServer(handler); //require http server, and create server with function handler()
+//var fs = require('fs'); //require filesystem module
+var io = require('socket.io')(http) //require socket.io module and pass the http object (server)
+
+//http.listen(8080); //listen to port 8080
+
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  var lightvalue = 0; //static variable for current status
+  socket.on('light', function(data) { //get light switch status from client
+    lightvalue = data;
+    if (lightvalue) {
+      console.log(lightvalue); //turn LED on or off, for now we will just show it in console.log
+    }
+  });
+}); 
