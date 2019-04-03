90b76132245aacb34e060754d7000c939125aaec
naflaherty <naf3@hw.ac.uk>
2019-04-03 15:36:04 +0100

    Working LED stays on

--- update.cpp
+++ update.cpp
@@ -15,6 +15,8 @@ int main(int argc, char** argv)
     // LED is connected on physical pin 11. Ensure
     // this pin is set to be an Output.
     pinMode(0, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
 
     // Ensure the LED is ON initially
     int state = HIGH;
@@ -24,30 +26,36 @@ int main(int argc, char** argv)
     // Read input on this pin
     pinMode(INPUT_PIN, INPUT);
 
-
-    while(true)
-    {
+    int flag = 0;
+    //while(true)
+    //{
         // As soon as we dedect an input, log and quit.
-        if(digitalRead(INPUT_PIN) == HIGH)
-		{
-			printf("Button is pressed!\n");	
-			//break;
-			
-			//switch on LEDs
-			  digitalWrite(0, state); //physical pin 11
-			  digitalWrite(3, state); //physical pin 15
-			  digitalWrite(4, state); //physical pin 16
-			  sleep(1);
-		}
-		else {
-		     digitalWrite(0, LOW);
-		      digitalWrite(3, LOW);
-		       digitalWrite(4, LOW);
-		     printf("else statement \n");
-		     sleep(1);
-		 }
-		
+    if(digitalRead(INPUT_PIN) == HIGH){
+	int flag = 1;
     }
+    
+	while(flag ==1)
+	{
+	printf("Button is pressed!\n");	
+	//break;
+			
+	//switch on LEDs
+	digitalWrite(0, state); //physical pin 11
+	digitalWrite(3, state); //physical pin 15
+	digitalWrite(4, state); //physical pin 16
+	sleep(1);
+	}
+    
+    if (digitalRead(INPUT_PIN) == LOW) {
+	flag ==0;
+	digitalWrite(0, LOW);
+	digitalWrite(3, LOW);
+	digitalWrite(4, LOW);
+	printf("else statement \n");
+	sleep(1);
+    }
+		
+    //}
    
 // Exit program
     return 0;
---
+++ working_LED_Stays_On.cpp
@@ -0,0 +1,73 @@
+//https://nickcullen.net/blog/raspberry-pi-tutorials/raspberry-pi-c-push-buttons-reading-gpio-input/
+//https://www.npmjs.com/package/onoff
+
+#include <stdio.h>
+#include <wiringPi.h>
+
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    // Intialize the wiringPi Library
+    wiringPiSetup();
+     
+    pinMode(0, OUTPUT); // LED is connected on physical pin 11.
+    pinMode(3, OUTPUT); // LED is connected on physical pin 15.
+    pinMode(4, OUTPUT); // LED is connected on physical pin 16.
+
+    // Ensure the LED is ON initially
+    int state = LOW;
+	
+	
+    const int INPUT_PIN = 1; //This the physical pin the switch is connected to. Pin 12
+    // Read input on this pin
+    pinMode(INPUT_PIN, INPUT);    
+	  
+    while(true)    
+    {
+        // As soon as we dedect an input, log and quit.
+       if((digitalRead(INPUT_PIN) == HIGH) && (state == HIGH))
+	{
+	    printf("Button is pressed!\n");
+	    state = LOW;		
+	    
+	    //switch on LEDs
+	    digitalWrite(0, state); //physical pin 11
+	    digitalWrite(3, state); //physical pin 15
+	    digitalWrite(4, state); //physical pin 16
+	    sleep(1);
+		
+	}
+    
+	else if((digitalRead(INPUT_PIN) == HIGH) && (state == LOW))
+	{
+	    printf("HIGH LOW\n");//
+	    state = HIGH;		
+	    
+	    //switch on LEDs
+	    digitalWrite(0, state); //physical pin 11
+	    digitalWrite(3, state); //physical pin 15
+	    digitalWrite(4, state); //physical pin 16
+	    sleep(1);
+
+    }
+    
+    else if ((digitalRead(INPUT_PIN) == LOW) && (state == LOW))
+    {
+	    printf("idling!\n"); //
+	    state = LOW;		
+	    
+	    //switch on LEDs
+	    digitalWrite(0, state); //physical pin 11
+	    digitalWrite(3, state); //physical pin 15
+	    digitalWrite(4, state); //physical pin 16
+	    sleep(1);
+		
+    }
+}
+
+    
+   digitalWrite(0, LOW);
+// Exit program
+    return 0;
+}
